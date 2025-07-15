f480571ec1b51d64e5d4c1648299d7f42037551a
Saravanan K <153268223+SaravananK03@users.noreply.github.com>
2025-07-15 14:25:35 +0530

    Add a authentication for Login and Logout

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "User Logged In")
+            return redirect('home')
+        else:
+            messages.success(request, "Login Error.. Please Try Again Later")
+            return redirect('home')
+
+
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "User Logged Out..")
+    return redirect('home')
